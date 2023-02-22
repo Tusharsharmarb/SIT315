5620282c4b5ff1382f2bb290ce06b70b18171fb9
Tusharsharmarb <105912219+Tusharsharmarb@users.noreply.github.com>
2023-02-22 15:43:36 +0530

    Update Task 1.1P.ino

--- Module1/Task 1.1P.ino
+++ Module1/Task 1.1P.ino
@@ -1,33 +1,36 @@
-const uint8_t BTN_PIN = 2;    // Define the digital pin number for the button input
-const uint8_t LED_PIN = 13;   // define the digital pin number for the LED output
-
-uint8_t buttonPrevState = LOW;    // stores the previous state of buttons
-uint8_t ledState = LOW;           // stores the current state of LED
-
+int ledPin = 13;                // choose the pin for the LED
+int inputPin = 2;               // choose the input pin (for PIR sensor)
+int pirState = LOW;             // at start, assume no motion detected
+int val = 0;                    // variable for reading the pin status
+ 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(BTN_PIN, INPUT_PULLUP);     // Sets the button pin as the input 
-  pinMode(LED_PIN, OUTPUT);       // Sets the LED pin as the output
-  Serial.begin(9600);           // Initiate the serial communication
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT);     // declare sensor as input
+ 
+  Serial.begin(9600);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  uint8_t buttonState = digitalRead(BTN_PIN); // Read the state of the button and store it in buttonState
-
-  Serial.print("buttonState: ");
-  Serial.println(buttonState); //print the values
-  Serial.print("buttonPrevState: ");
-  Serial.println(buttonPrevState);
-  Serial.print("Led State: ");
-  Serial.println(ledState);
-
-  if(buttonState != buttonPrevState) // check if current button state is different from previous button state
+ 
+void loop(){
+  val = digitalRead(inputPin);  // read input value
+  
+  if (val == HIGH)	// check if the input is HIGH
+  {            
+    digitalWrite(ledPin, HIGH);  // turn LED ON
+	
+    if (pirState == LOW) 
+	{
+      Serial.println("Motion detected!");	// print on output change
+      pirState = HIGH;
+    }
+  } 
+  else 
   {
-    ledState = !ledState;     // Toggle the state of the LED
-    digitalWrite(LED_PIN, ledState);    // Update the LED State on the LED pin
+    digitalWrite(ledPin, LOW); // turn LED OFF
+	
+    if (pirState == HIGH)
+	{
+      Serial.println("Motion ended!");	// print on output change
+      pirState = LOW;
+    }
   }
-
-  buttonPrevState = buttonState; // Stores the current button state as the previous button state for next iterations of the loop
-  delay(500); // delay the loop for 500 milliseconds
 }
